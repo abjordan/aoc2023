ec91a96d3d6706dfe8b728be978cc70c5a402303
Alex Jordan <abjordan@gmail.com>
2023-12-17 15:39:26 -0500

    Day 11, part B. Didn't even cheat!

--- day11/day11.py
+++ day11/day11.py
@@ -59,7 +59,62 @@ def part_a(field):
     distance_sum = 0
     for p in galaxy_pairs:
         # The metric here is Manhattan distance 
-        distance_sum += abs(p[0][0] - p[1][0]) + abs(p[0][1] - p[1][1])
+        d = abs(p[0][0] - p[1][0]) + abs(p[0][1] - p[1][1])
+        distance_sum += d
+        #print(f"({p[0][0]},{p[0][1]}) --> ({p[1][0]},{p[1][1]}) = {d}")
+    return distance_sum
+
+def between(bound_a, bound_b, query):
+    #print(f"{bound_a} < {query} < {bound_b}")
+    if bound_a < bound_b:
+        return ((query > bound_a) and (query < bound_b))
+    elif bound_b < bound_a:
+        return ((query > bound_b) and (query < bound_a))
+    else:
+        # This smells like a bug waiting to happen
+        return False
+
+
+def part_b(field, expansion_factor=1):
+    # The trick here is to find the rows and columns that are empty
+    # and then check to see how many of them are between you in each
+    # dimension. You can just add that much to each distance as you
+    # calculate the Manhattan distance, rather than generating the actual
+    # starfield.
+    row_sums = np.sum(field, axis=1)
+    col_sums = np.sum(field, axis=0)
+    
+    expansion_factor -= 1   # If it "grows by 10", you add 9 spaces
+
+    empty_rows = []
+    empty_cols = []
+    for r_idx, row in enumerate(row_sums):
+        if row == 0:
+            empty_rows.append(r_idx)
+    for c_idx, col in enumerate(col_sums):
+        if col == 0:
+            empty_cols.append(c_idx)
+    print("Rows: ", empty_rows)
+    print("Cols: ", empty_cols)
+
+    galaxy_coords = np.transpose(np.nonzero(field))
+    galaxy_pairs = itertools.combinations(galaxy_coords, 2)
+
+    distance_sum = 0
+    for p in galaxy_pairs:
+        m_dist = abs(p[0][0] - p[1][0]) + abs(p[0][1] - p[1][1])
+        # For each empty row between the galaxies, add the expansion factor
+        for er in empty_rows:
+            if between(p[0][0], p[1][0], er):
+                #print("_")
+                m_dist += expansion_factor
+        for ec in empty_cols:
+            if between(p[0][1], p[1][1], ec):
+                #print("_")
+                m_dist += expansion_factor
+        distance_sum += m_dist
+        #print(f"({p[0][0]},{p[0][1]}) --> ({p[1][0]},{p[1][1]}) = {m_dist}")
+        #break
     return distance_sum
 
 if __name__ == "__main__":
@@ -78,4 +133,17 @@ if __name__ == "__main__":
         print_field(field)
         print('-----------------------------')
         result_a = part_a(field)
-        print(f"Sum of galaxy distances is {result_a}")
+        print(f"Part A: Sum of galaxy distances is {result_a}")
+
+        print('-----------------------------')
+        result_b = part_b(field, 2)
+        print(f"Part B: Sum of 1x expanded distances is {result_b}")
+
+        result_b = part_b(field, 10)
+        print(f"Part B: Sum of 10x expanded distances is {result_b}")
+
+        result_b = part_b(field, 100)
+        print(f"Part B: Sum of 100x expanded distances is {result_b}")
+
+        result_b = part_b(field, 1000000)
+        print(f"Part B: Sum of 100x expanded distances is {result_b}")
